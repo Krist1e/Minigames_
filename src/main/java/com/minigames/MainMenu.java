9abb6ecb9862d42e557dc84b68cd9d44728c80b0
Kristina Alekseeva <kris_lexie@mail.ru>
2021-10-28 15:20:16 +0300

    Updated GUI

--- src/main/java/com/minigames/MainMenu.java
+++ src/main/java/com/minigames/MainMenu.java
@@ -11,7 +11,7 @@ public class MainMenu extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainMenu.class.getResource("SceneLayout.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1200, 700);
+        Scene scene = new Scene(fxmlLoader.load(), 1100, 700);
         stage.setTitle("Minigames");
         stage.setScene(scene);
         stage.setResizable(false);
