ee586c4119de6901940281de5649fe9081dc314d
Kristina Alekseeva <kris_lexie@mail.ru>
2021-10-30 19:02:03 +0300

    Updated GUI

--- src/main/java/com/minigames/Controller.java
+++ src/main/java/com/minigames/Controller.java
@@ -1,10 +1,18 @@
 package com.minigames;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
 
     @FXML
     private Button ChooseSettingsButton;
@@ -20,4 +28,20 @@ public class Controller {
 
     }
 
+    @FXML
+    void SettingsClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void TicTacToeClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
 }
+
