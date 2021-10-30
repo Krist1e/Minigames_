2e682e779f98f88e824fa679469dd0f37e4648be
Kristina Alekseeva <kris_lexie@mail.ru>
2021-10-30 21:34:35 +0300

    Created new stage

--- src/main/java/com/minigames/Controller.java
+++ src/main/java/com/minigames/Controller.java
@@ -1,23 +1,13 @@
 package com.minigames;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 
 public class Controller {
-
-        private Stage stage;
-        private Scene scene;
-        private Parent root;
 
     @FXML
     private ResourceBundle resources;
@@ -35,7 +25,7 @@ public class Controller {
     private Button TicTacToeStartButton;
 
     @FXML
-    void MinesweeperClick(ActionEvent event){
+    void MinesweeperClick(MouseEvent event) {
 
     }
 
@@ -63,4 +53,5 @@ public class Controller {
     }
 
 }
+
 
