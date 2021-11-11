69f59668382a04e089cf12ce1b8b72cdf5fe9155
AndreKlim1 <andreyklimovich159@gmail.com>
2021-11-11 21:59:53 +0300

    MineSweeper foundation

--- src/main/java/com/minigames/MinesweeperHardController.java
+++ src/main/java/com/minigames/MinesweeperHardController.java
@@ -1,4 +1,37 @@
 package com.minigames;
 
-public class MinesweeperHardController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.TilePane;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MinesweeperHardController implements Initializable {
+
+
+    @FXML
+    private TilePane tilePane;
+
+    Button[] AllTiles;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        AllTiles = new Button[352];
+        for (int i = 0; i<352; i++) {
+            Button tile = new Button();
+            tile.setLayoutX(122+i*100);
+            tile.setLayoutY(64);
+            tile.setPrefSize(48,42);
+            tile.setText(String.valueOf(i));
+            tilePane.getChildren().add(tile);
+            AllTiles[i] = tile;
+        }
+        for (int b = 1; b<72; b++) {
+            int random = (int )(Math.random()*352);
+            AllTiles[random].setText("X");
+
+        }
+    }
 }
