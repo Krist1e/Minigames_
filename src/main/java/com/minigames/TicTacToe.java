9ef1ea0f5839a8a6f334cfd102930cafdb45d570
Kristina Alekseeva <kris_lexie@mail.ru>
2021-11-29 23:36:05 +0300

    Updated

--- src/main/java/com/minigames/TicTacToe.java
+++ src/main/java/com/minigames/TicTacToe.java
@@ -2,17 +2,11 @@ package com.minigames;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
