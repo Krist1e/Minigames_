9910d29cf4ae8138577ee1eb5ab0b9b7f6edf571
AndreKlim1 <andreyklimovich159@gmail.com>
2021-12-02 01:23:55 +0300

    Javadoc Vol.2

---
+++ src/main/resources/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.minigames","l":"MainMenu"},{"p":"com.minigames","l":"MainMenuController"},{"p":"com.minigames","l":"Minesweeper"},{"p":"com.minigames","l":"MinesweeperDifficultyController"},{"p":"com.minigames","l":"SettingsController"},{"p":"com.minigames","l":"TicTacToe"}]
